e34c02d8dc3d236f5c71f95dfce1b82097587aad
Mohamed Taha <m.ahmed.taha251@gmail.com>
2019-01-10 09:27:02 +0200

    Add ListView and Floating button

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import './product_manager.dart';
+import './DetailedScreen.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,8 +14,54 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('EasyList'),
         ),
-        body: ProductManager('Food Tester'),
+        body: getListView(),
+          floatingActionButton: new FloatingActionButton(
+              elevation: 0.0,
+              child: new Icon(Icons.add),
+              tooltip: 'Add one More Item',
+              backgroundColor: Colors.deepOrange,
+              onPressed: (){
+
+              }
+          )
       ),
     );
   }
+}
+
+void showSnackBar(BuildContext context,String item)
+{
+  var snackbar=SnackBar(content: Text("you just tapped $item"));
+  Scaffold.of(context).showSnackBar(snackbar);
+}
+
+void openDetailedScreen(BuildContext context)
+{
+  Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => DetailedScreen()),
+  );
+}
+
+List<String> getListItem()
+{
+  return List<String>.generate(1000, (counter)=>"Item$counter");
+}
+
+Widget getListView(){
+  var listItems=getListItem();
+  var listView=ListView.builder(
+    itemBuilder: (context,index){
+      return ListTile(
+        leading: Icon(Icons.arrow_right),
+        title: Text(listItems[index]),
+      onTap: () {
+        showSnackBar(context,listItems[index]);
+        //openDetailedScreen(context);
+
+      },
+      );
+    }
+  );
+return listView;
 }
